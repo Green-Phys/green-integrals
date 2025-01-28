7c6824f37186a4e343d4b40abc74cc9e2ec06ada
Gaurav Harsha <gauravharsha05@gmail.com>
2025-01-28 18:55:26 -0500

    update unit tests for shared mem read for Silicon; add test for integral version

--- test/test_hash.cpp
+++ test/test_hash.cpp
@@ -6,17 +6,28 @@
 #include <iomanip>
 
 
+// For these tests of reading integrals as chunks, we use cooked up density-fitted integrals.
+// system           :   Silicon
+// k-mesh           :   2x2x2
+// basis            :   gth-dzvp-molopt-sr
+// pseudo           :   gth-pbe
+// size per file    :   1MB
+// beta (df params) :   2.0
+// diffuse_cutoff   :   0.3
+
+using namespace std::string_literals;
+
 TEST_CASE("HashOfKeys","[ReadingSI]") {
-  int chunks_per_file=336;
-  int total_files=36;
-  int nao=26;
-  int naux=200;
+  int chunks_per_file=18;
+  int total_files=2;
+  int nao=8;
+  int naux=36;
   int number_of_keys=chunks_per_file*total_files;
+  std::string df_path=TEST_PATH+"/Si/df_int"s;
 
   unsigned char digest[SHA_DIGEST_LENGTH];
 
-
-  chunk_reader c(TEST_PATH, number_of_keys, naux, nao); //test these numbers
+  chunk_reader c(df_path, number_of_keys, naux, nao); //test these numbers
   buffer b(c.element_size(), number_of_keys, 100, &c, 0, false);
   if(b.shmem_rank()==0){
   for(int i=0;i<5;++i){
---
+++ test/test_integral_version.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2023 University of Michigan
+ *
+ */
+
+#include <green/symmetry/symmetry.h>
+#include "green/integrals/df_integral_t.h"
+
+#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include "catch2/matchers/catch_matchers.hpp"
+
+
+using green::integrals::df_integral_t;
+
+TEST_CASE("Integral version", "[Input]") {
+  // SKIP("This test is disabled until all integral data and throw statements are updated");
+  auto        p             = green::params::params("DESCR");
+  std::string input_file    = TEST_PATH + "/Input/input.h5"s;
+  std::string df_int_path_1 = TEST_PATH + "/Input/df_int"s;
+  std::string df_int_path_2 = TEST_PATH + "/Input/df_int_x"s;
+  std::string df_int_path_3 = TEST_PATH + "/Input/df_int_y"s;
+  std::string args =
+      "test --restart 0 --itermax 2 --E_thr 1e-13 --mixing_type G_MIXING --mixing_weight 0.8 --input_file=" + input_file +
+      " --BETA 100 --verbose=1 --dfintegral_file=" + df_int_path_1 +
+      " --dfintegral_hf_file=" + df_int_path_2;
+  green::symmetry::define_parameters(p);
+  p.parse(args);
+  int verbose = 1;
+  green::symmetry::brillouin_zone_utils<green::symmetry::inv_symm_op> bz_utils(p);
+  // TODO: Transition std::cerr in df_buffered_reader.h to green::integrals::integrals_outdated_input
+  // REQUIRE_THROWS_AS(df_integral_t(df_int_path_1, 2, 36, bz_utils, verbose), green::integrals::integrals_outdated_input);
+  // REQUIRE_THROWS_AS(df_integral_t(df_int_path_2, 2, 36, bz_utils, verbose), green::integrals::integrals_outdated_input);
+  REQUIRE_NOTHROW(df_integral_t(df_int_path_3, 2, 36, bz_utils, verbose));
+}
